2f97d22ce19af2151d43ebe37bc127a64b898fbc
patrickstowell <john.patrick.stowell@gmail.com>
2018-07-18 16:52:53 +0100

    Updated Vertex Grid to save poca results.

--- src/chance/VertexGrid.cc
+++ src/chance/VertexGrid.cc
@@ -72,10 +72,17 @@ void VertexGrid::Write() {
   TH3F* hist75 = new TH3F("hist75", "hist75", fXnbins, fXMIN, fXMAX, fYnbins, fYMIN, fYMAX, fZnbins, fZMIN, fZMAX);
   TH3I* histNS = new TH3I("histNS", "histNS", fXnbins, fXMIN, fXMAX, fYnbins, fYMIN, fYMAX, fZnbins, fZMIN, fZMAX);
   TH3F* histBS = new TH3F("histBS", "histBS", fXnbins, fXMIN, fXMAX, fYnbins, fYMIN, fYMAX, fZnbins, fZMIN, fZMAX);
+  TH3F* histPC = new TH3F("histPC", "histPC", fXnbins, fXMIN, fXMAX, fYnbins, fYMIN, fYMAX, fZnbins, fZMIN, fZMAX);
 
   int totalbins = histNS->GetNbinsX() * histNS->GetNbinsY() * histNS->GetNbinsZ();
   int printbins = totalbins / 100;
   int curntbins = 0;
+
+
+  TTree* tree = new TTree("T","T");
+  double ClusterInfo[12];
+  tree->Branch("clusters",&ClusterInfo, "cx/D:cy:cz:tracks:discr:isextremal:asr025:asr050:asr075:poca:x:y:z");
+  
 
   for (int i = 0; i < histNS->GetNbinsX(); i++) {
     double x = histNS->GetXaxis()->GetBinCenter(i + 1);
@@ -93,43 +100,81 @@ void VertexGrid::Write() {
         std::vector<EventVertex> vertices = fVertices[voxelid];
         if (vertices.size() < 2) continue;
 
-        std::vector<double> disc;
+	ClusterInfo[0] = i;
+	ClusterInfo[1] = j;
+	ClusterInfo[2] = k;
+	
+	ClusterInfo[10] = x;
+	ClusterInfo[11] = y;
+	ClusterInfo[12] = z;
+
+	ClusterInfo[3] = vertices.size();
+	histNS->SetBinContent(i + 1, j + 1, k + 1, vertices.size());
+
+	ClusterInfo[5] = 0.0; // Is Extremal?
+
+	// Bristol Algorithm
+	double bristoldisc = 0.0;
+	std::vector<double> disc;
+        for (int q = 0; q < vertices.size(); q++) {
+	  for (int p = 0; p < vertices.size(); p++) {
+	    if (p <= q) continue;
+	    EventVertex v1 = vertices[q];
+	    EventVertex v2 = vertices[p];
+	    float distance = sqrt(pow(v1.x - v2.x, 2) + pow(v1.y - v2.y, 2) + pow(v1.z - v2.z, 2) );
+	    disc.push_back(distance / (v1.th * v1.mom) / (v2.th * v2.mom));
+	  }
+	}
+	std::sort(disc.begin(), disc.end());
+	
+	double discrim = disc[disc.size() / 2];
+	if (discrim != 0.0){
+	  discrim = log(discrim);
+	}
+	ClusterInfo[4] = discrim;
+	histBS->SetBinContent(i + 1, j + 1, k + 1, discrim);
+
+	// Angular Statistics Algorithm 
+	disc.clear();
         for (int q = 0; q < vertices.size(); q++) {
           disc.push_back(vertices[q].thx * vertices[q].mom);
           disc.push_back(vertices[q].thy * vertices[q].mom);
         }
         std::sort(disc.begin(), disc.end());
-        
-        std::cout << "MEDIAN : " << disc[0.75 * disc.size()] << std::endl;
-        hist25->SetBinContent(i + 1, j + 1, k + 1, disc[0.25 * disc.size()]);
-        hist50->SetBinContent(i + 1, j + 1, k + 1, disc[0.50 * disc.size()]);
-        hist75->SetBinContent(i + 1, j + 1, k + 1, disc[0.75 * disc.size()]);
-        histNS->SetBinContent(i + 1, j + 1, k + 1, disc.size());
+  
+	ClusterInfo[6] =  disc[0.25 * disc.size()];
+        hist25->SetBinContent(i + 1, j + 1, k + 1, ClusterInfo[6]);
 
+	ClusterInfo[7] =  disc[0.50 * disc.size()];
+        hist50->SetBinContent(i + 1, j + 1, k + 1, ClusterInfo[7]);
 
-        double bristoldisc = 0.0;
-        disc.clear();
-        for (int q = 0; q < vertices.size(); q++) {
-           for (int p = 0; p < vertices.size(); p++) {
-             if (p <= q) continue;
-             EventVertex v1 = vertices[q];
-             EventVertex v2 = vertices[p];
-             float distance = sqrt(pow(v1.x - v2.x, 2) + pow(v1.y - v2.y, 2) + pow(v1.z - v2.z, 2) );
-             disc.push_back(distance / (v1.th * v1.mom) / (v2.th * v2.mom));
-           }
-         }
-         std::sort(disc.begin(), disc.end());
+	ClusterInfo[8] =  disc[0.75 * disc.size()];
+        hist75->SetBinContent(i + 1, j + 1, k + 1, ClusterInfo[8]);
 
-         histBS->SetBinContent(i + 1, j + 1, k + 1, disc[disc.size() / 2]);
-        fVertices[voxelid].clear();
+	// Point of closest approach algorithm
+	double angle = 0.0;
+	for (int q = 0; q < vertices.size(); q++) {
+	  angle += vertices[q].th;
+	}
+	angle /= double(vertices.size());
+	
+	ClusterInfo[9] = angle;
+	histPC->SetBinContent(i+1,j+1,k+1,angle);
 
+	// Fill outputs
+	tree->Fill();
+
+	// Remove voxel
+	fVertices[voxelid].clear();
       }
     }
   }
 
+  tree->Write();
   hist25->Write();
   hist50->Write();
   hist75->Write();
+  histPC->Write();
   histNS->Write();
   histBS->Write();
 
