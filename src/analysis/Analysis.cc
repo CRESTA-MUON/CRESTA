b3a9d8e337d96128a23c95e717ebf5da7477afa0
patrickstowell <john.patrick.stowell@gmail.com>
2018-08-02 13:23:04 +0100

    Added average density calculations to voxel mc true histograms

--- src/analysis/Analysis.cc
+++ src/analysis/Analysis.cc
@@ -58,23 +58,42 @@ void Analysis::BuildMCMap() {
 
   double xmin = configuration.GetD("grid_minx");
   double xmax = configuration.GetD("grid_maxx");
-  int nbinsx = ((xmax - xmin)/grid_size);
+  int nbinsx = ((xmax - xmin) / grid_size);
 
   double ymin = configuration.GetD("grid_miny");
   double ymax = configuration.GetD("grid_maxy");
-  int nbinsy = ((ymax - ymin)/grid_size);
+  int nbinsy = ((ymax - ymin) / grid_size);
 
   double zmin = configuration.GetD("grid_minz");
   double zmax = configuration.GetD("grid_maxz");
-  int nbinsz = ((zmax - zmin)/grid_size);
+  int nbinsz = ((zmax - zmin) / grid_size);
 
-  std::cout << "Building density hist" << std::endl;
-  TH3D* density_hist = new TH3D("density_hist","density_hist", nbinsx, xmin, xmax, nbinsy, ymin, ymax, nbinsz, zmin, zmax);
+  int nthrows = 100;
+  if (configuration.Has("nthrows")) nthrows = configuration.GetI("nthrows");
 
-  std::cout << "Getting Navigator" << std::endl;
+  std::cout << "Building voxel histograms." << std::endl;
+  TH3D* density_hist = new TH3D("density_hist", "Voxel Density (g/cm3)", nbinsx, xmin, xmax, nbinsy, ymin, ymax, nbinsz, zmin, zmax);
+  TH3D* atomspv_hist = new TH3D("atomspv_hist", "Voxel Atoms Per Volume", nbinsx, xmin, xmax, nbinsy, ymin, ymax, nbinsz, zmin, zmax);
+  TH3D* elecspv_hist = new TH3D("elecspv_hist", "Voxel Electrons Per Volume", nbinsx, xmin, xmax, nbinsy, ymin, ymax, nbinsz, zmin, zmax);
+  TH3D* elecden_hist = new TH3D("elecden_hist", "Voxel Electron Density", nbinsx, xmin, xmax, nbinsy, ymin, ymax, nbinsz, zmin, zmax);
+  TH3D* radlens_hist = new TH3D("radlens_hist", "Voxel Radiation Length (mm)", nbinsx, xmin, xmax, nbinsy, ymin, ymax, nbinsz, zmin, zmax);
+  TH3D* nuclens_hist = new TH3D("nuclens_hist", "Voxel Nuclear Length (mm)", nbinsx, xmin, xmax, nbinsy, ymin, ymax, nbinsz, zmin, zmax);
+  // TH3D* atomicZ_hist = new TH3D("atomicZ_hist", "Voxel Atomic Z", nbinsx, xmin, xmax, nbinsy, ymin, ymax, nbinsz, zmin, zmax);
+  // TH3D* atomicA_hist = new TH3D("atomicA_hist", "Voxel Atomic A", nbinsx, xmin, xmax, nbinsy, ymin, ymax, nbinsz, zmin, zmax);
+
+  TH3D* density_ravg = new TH3D("density_ravg", "Rand. Avg. Density (g/cm3)", nbinsx, xmin, xmax, nbinsy, ymin, ymax, nbinsz, zmin, zmax);
+  TH3D* atomspv_ravg = new TH3D("atomspv_ravg", "Rand. Avg. Atoms Per Volume", nbinsx, xmin, xmax, nbinsy, ymin, ymax, nbinsz, zmin, zmax);
+  TH3D* elecspv_ravg = new TH3D("elecspv_ravg", "Rand. Avg. Electrons Per Volume", nbinsx, xmin, xmax, nbinsy, ymin, ymax, nbinsz, zmin, zmax);
+  TH3D* elecden_ravg = new TH3D("elecden_ravg", "Rand. Avg. Electron Density", nbinsx, xmin, xmax, nbinsy, ymin, ymax, nbinsz, zmin, zmax);
+  TH3D* radlens_ravg = new TH3D("radlens_ravg", "Rand. Avg. Radiation Length (mm)", nbinsx, xmin, xmax, nbinsy, ymin, ymax, nbinsz, zmin, zmax);
+  TH3D* nuclens_ravg = new TH3D("nuclens_ravg", "Rand. Avg. Nuclear Length (mm)", nbinsx, xmin, xmax, nbinsy, ymin, ymax, nbinsz, zmin, zmax);
+  // TH3D* atomicZ_ravg = new TH3D("atomicZ_ravg", "Rand. Avg. Atomic Z", nbinsx, xmin, xmax, nbinsy, ymin, ymax, nbinsz, zmin, zmax);
+  // TH3D* atomicA_ravg = new TH3D("atomicA_ravg", "Rand. Avg. Atomic A", nbinsx, xmin, xmax, nbinsy, ymin, ymax, nbinsz, zmin, zmax);
+  TH3D* counter_ravg = new TH3D("counter_ravg", "Rand. Avg. Counter", nbinsx, xmin, xmax, nbinsy, ymin, ymax, nbinsz, zmin, zmax);
+
   G4Navigator* nav =  G4TransportationManager::GetTransportationManager()->GetNavigatorForTracking();
-  std::cout << "Getting nav data " << nav << std::endl;
   for (int i = 0; i < nbinsx; i++) {
+    if (i % 5 == 0) std::cout << "--> Processed " << i << "/" << nbinsx << " 2D slices." << std::endl;
     for (int j = 0; j < nbinsy; j++) {
       for (int k = 0; k < nbinsz; k++) {
 
@@ -82,7 +101,7 @@ void Analysis::BuildMCMap() {
         double y = ymin + (j + 0.5) * grid_size;
         double z = zmin + (k + 0.5) * grid_size;
 
-        G4VPhysicalVolume* phys = nav->LocateGlobalPointAndSetup(G4ThreeVector(x*mm, y*mm, z*mm));
+        G4VPhysicalVolume* phys = nav->LocateGlobalPointAndSetup(G4ThreeVector(x * mm, y * mm, z * mm));
         if (!phys) continue;
 
         G4LogicalVolume* log = phys->GetLogicalVolume();
@@ -93,13 +112,72 @@ void Analysis::BuildMCMap() {
 
         G4double density = mat->GetDensity() / (g / cm3);
 
-        density_hist->SetBinContent(i,j,k,density);
+        density_hist->SetBinContent(i, j, k, density);
+        atomspv_hist->SetBinContent(i, j, k, mat->GetTotNbOfAtomsPerVolume());
+        elecspv_hist->SetBinContent(i, j, k, mat->GetTotNbOfElectPerVolume());
+        elecden_hist->SetBinContent(i, j, k, mat->GetElectronDensity());
+        radlens_hist->SetBinContent(i, j, k, mat->GetRadlen());
+        nuclens_hist->SetBinContent(i, j, k, mat->GetNuclearInterLength());
+        // atomicZ_hist->SetBinContent(i, j, k, mat->GetZ());
+        // atomicA_hist->SetBinContent(i, j, k, mat->GetA());
 
+        for (int t = 0; t < nthrows; t++) {
+          x = xmin + (i + G4UniformRand()) * grid_size;
+          y = ymin + (j + G4UniformRand()) * grid_size;
+          z = zmin + (k + G4UniformRand()) * grid_size;
+
+          G4VPhysicalVolume* phys = nav->LocateGlobalPointAndSetup(G4ThreeVector(x * mm, y * mm, z * mm));
+          if (!phys) continue;
+
+          G4LogicalVolume* log = phys->GetLogicalVolume();
+          if (!log) continue;
+
+          G4Material* mat = log->GetMaterial();
+          if (!mat) continue;
+
+          G4double density = mat->GetDensity() / (g / cm3);
+
+          density_ravg->SetBinContent(i, j, k, density_ravg->GetBinContent(i,j,k) + density);
+          atomspv_ravg->SetBinContent(i, j, k, atomspv_ravg->GetBinContent(i,j,k) + mat->GetTotNbOfAtomsPerVolume());
+          elecspv_ravg->SetBinContent(i, j, k, elecspv_ravg->GetBinContent(i,j,k) + mat->GetTotNbOfElectPerVolume());
+          elecden_ravg->SetBinContent(i, j, k, elecden_ravg->GetBinContent(i,j,k) + mat->GetElectronDensity());
+          radlens_ravg->SetBinContent(i, j, k, radlens_ravg->GetBinContent(i,j,k) + mat->GetRadlen());
+          nuclens_ravg->SetBinContent(i, j, k, nuclens_ravg->GetBinContent(i,j,k) + mat->GetNuclearInterLength());
+          // atomicZ_ravg->SetBinContent(i, j, k, atomicZ_ravg->GetBinContent(i,j,k) + mat->GetZ());
+          // atomicA_ravg->SetBinContent(i, j, k, atomicA_ravg->GetBinContent(i,j,k) + mat->GetA());
+          counter_ravg->SetBinContent(i, j, k, counter_ravg->GetBinContent(i,j,k) + 1);
+        }
       }
     }
   }
-  // Write the histograms
+
+  // Write the voxel histograms
   density_hist->Write();
+  atomspv_hist->Write();
+  elecspv_hist->Write();
+  elecden_hist->Write();
+  radlens_hist->Write();
+  nuclens_hist->Write();
+  // atomicZ_hist->Write();
+  // atomicA_hist->Write();
+
+  density_ravg->Divide(counter_ravg);
+  atomspv_ravg->Divide(counter_ravg);
+  elecspv_ravg->Divide(counter_ravg);
+  elecden_ravg->Divide(counter_ravg);
+  radlens_ravg->Divide(counter_ravg);
+  nuclens_ravg->Divide(counter_ravg);
+  // atomicZ_ravg->Divide(counter_ravg);
+  // atomicA_ravg->Divide(counter_ravg);
+
+  density_ravg->Write();
+  atomspv_ravg->Write();
+  elecspv_ravg->Write();
+  elecden_ravg->Write();
+  radlens_ravg->Write();
+  nuclens_ravg->Write();
+  // atomicZ_ravg->Write();
+  // atomicA_ravg->Write();
 
   // Clean up
   mapfile->Close();
