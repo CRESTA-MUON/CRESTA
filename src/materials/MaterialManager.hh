40786668296b7c172142bfc842d820a8483aba82
patrickstowell <john.patrick.stowell@gmail.com>
2018-08-20 11:00:04 +0100

    Cleaned up material folder

--- src/materials/MaterialManager.hh
+++ src/materials/MaterialManager.hh
@@ -1,12 +1,39 @@
-#ifndef __MATERIALS_MANAGER_HH__
-#define __MATERIALS_MANAGER_HH__
+// ********************************************************************
+// * License and Disclaimer                                           *
+// *                                                                  *
+// * The  Geant4 software  is  copyright of the Copyright Holders  of *
+// * the Geant4 Collaboration.  It is provided  under  the terms  and *
+// * conditions of the Geant4 Software License,  included in the file *
+// * LICENSE and available at  http://cern.ch/geant4/license .  These *
+// * include a list of copyright holders.                             *
+// *                                                                  *
+// * Neither the authors of this software system, nor their employing *
+// * institutes,nor the agencies providing financial support for this *
+// * work  make  any representation or  warranty, express or implied, *
+// * regarding  this  software system or assume any liability for its *
+// * use.  Please see the license in the file  LICENSE  and URL above *
+// * for the full disclaimer and the limitation of liability.         *
+// *                                                                  *
+// * This  code  implementation is the result of  the  scientific and *
+// * technical work of the GEANT4 collaboration.                      *
+// * By using,  copying,  modifying or  distributing the software (or *
+// * any work based  on the software)  you  agree  to acknowledge its *
+// * use  in  resulting  scientific  publications,  and indicate your *
+// * acceptance of all terms of the Geant4 Software license.          *
+// ********************************************************************
+#ifndef __COSMIC_MaterialManager_hh__
+#define __COSMIC_MaterialManager_hh__
 
+// System Headers
 #include <iostream>
 
+// Cosmic Headers
 #include "db/DBTable.hh"
 
+// Forward Declarations
 class G4Material;
 class G4Element;
+class G4VisAttributes;
 
 namespace COSMIC {
 
@@ -25,7 +52,6 @@ G4VisAttributes* GetVisForMaterial(DBTable table);
 
 /// Get vis just based on material database name
 G4VisAttributes* GetVisForMaterial(std::string name);
-
 
 } // - namespace MaterialFactory
 } // - namespace COSMIC
